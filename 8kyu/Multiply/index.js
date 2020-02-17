5a6dc7af9a26ff14bd93a1031fac5d56f340b1b0
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2020-02-17 14:45:18 +0100

    first kata added

---
+++ 8kyu/Multiply/index.js
@@ -0,0 +1,14 @@
+// Description:
+// The code does not execute properly.Try to figure out why.
+
+// function multiply(a, b) {
+//     a * b
+// }
+
+// v1
+// function multiply(a, b) {
+//     return a * b
+// }
+
+// v2
+const multiply = (a, b) => a * b;
