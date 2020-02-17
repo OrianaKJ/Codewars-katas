130e808b01007d4e4755e4f9ab05d3d58740cbae
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2020-02-17 14:59:19 +0100

    are you playing banjo kata added

---
+++ 8kyu/Are You Playing Banjo/index.js
@@ -0,0 +1,24 @@
+// Description:
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+// My Solutions:
+
+// v1
+// function areYouPlayingBanjo(name) {
+//     return (name.charAt(0) === "R" || name.charAt(0) === "r") ? `${name} plays banjo` : `${name} does not play banjo`;
+
+// }
+
+// v2
+function areYouPlayingBanjo(name) {
+    let splittedName = name.split('');
+    let firstLetter = splittedName[0];
+    return firstLetter.toUpperCase() === 'R' ? name + ' plays banjo' : name + ' does not play banjo';
+}
