6b8d03a985540da550d192ade12714c10022c96f
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2020-02-20 12:23:15 +0100

    grasshopper - grade book kata added

---
+++ 8kyu/Grasshopper - Grade book/index.js
@@ -0,0 +1,22 @@
+// Description:
+// Grade book
+// Complete the function so that it finds the mean of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+// My solution:
+function getGrade(s1, s2, s3) {
+    let sum = (s1 + s2 + s3) / 3;
+    return sum >= 90 ? "A" :
+        sum >= 80 && sum < 90 ? "B" :
+            sum >= 70 && sum < 80 ? "C" :
+                sum >= 60 && sum < 70 ? "D" :
+                    sum < 60 && sum >= 0 ? "F" :
+                        ''
+}
