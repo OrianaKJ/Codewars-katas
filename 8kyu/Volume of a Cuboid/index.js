e24d06441463713f10e3696571b4f3feb87f5573
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2020-02-20 15:55:59 +0100

    volume of cuboid kata added

---
+++ 8kyu/Volume of a Cuboid/index.js
@@ -0,0 +1,5 @@
+// Description:
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid.Write a function to help Bob with this calculation.
+
+// My solution:
+const getVolumeOfCuboid = (l, w, h) => l * w * h
