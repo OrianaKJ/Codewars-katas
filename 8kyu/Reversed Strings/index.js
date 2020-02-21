3e26823d92529f0e3ae17fd41761ced88340c67c
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2020-02-21 14:11:55 +0100

    reversed strings kata added

---
+++ 8kyu/Reversed Strings/index.js
@@ -0,0 +1,7 @@
+// Description:
+// Complete the solution so that it reverses the string value passed into it.
+
+// solution('world'); // returns 'dlrow'
+
+// My solution:
+const solution = str => str.split('').reverse().join('')
