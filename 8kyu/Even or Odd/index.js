be2f537661933ae250cf360c90db91189ed97370
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2020-02-21 13:42:59 +0100

    even or odd kata added

---
+++ 8kyu/Even or Odd/index.js
@@ -0,0 +1,5 @@
+// Description:
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// My solution:
+const even_or_odd = number => number % 2 === 0 ? 'Even' : 'Odd'
