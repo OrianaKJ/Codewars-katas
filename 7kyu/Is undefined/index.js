71516088b8c5159a7df0ed320cc4e7e90338494f
Oriana <oriana.koziorynska@gmail.com>
2020-03-13 21:30:43 +0100

    Is undefined kata added

---
+++ 7kyu/Is undefined/index.js
@@ -0,0 +1,8 @@
+// Description:
+// Is Undefined?
+// Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+// Task
+// Your task is to finish the isUndefined method which returns true if the value is undefined :)
+
+// My solution:
+const isUndefined = value => String(value).includes(undefined)
