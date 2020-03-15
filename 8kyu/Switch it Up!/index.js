c844ac06c5af670e72a1755561dbcf7654328430
Oriana <oriana.koziorynska@gmail.com>
2020-03-15 16:05:26 +0100

    switch it up kata added

---
+++ 8kyu/Switch it Up!/index.js
@@ -0,0 +1,9 @@
+// Description:
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// My solution:
+const switchItUp = number => ['Zero','One', 'Two', 'Three','Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number]
