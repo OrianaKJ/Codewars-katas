f8b50babfc2be4b99d134f477aaa133d27ffdf64
Oriana <oriana.koziorynska@gmail.com>
2020-03-23 19:55:32 +0100

    regex validate pin code kata added

---
+++ 7kyu/Regex validate PIN code/index.js
@@ -0,0 +1,20 @@
+// Description:
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// eg:
+
+// validatePIN("1234") === true
+// validatePIN("12345") === false
+// validatePIN("a234") === false
+
+// My solution:
+const validatePIN =pin => { 
+    if (pin.match(/^\d{4}$|^\d{6}$/) && (pin.length == 4 || pin.length == 6)){
+        return true
+    }
+    else {
+        return false;
+    }
+}
