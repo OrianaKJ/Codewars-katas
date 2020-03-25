4a6a785b256a25f60f014134cdba74450fb697d7
Oriana <oriana.koziorynska@gmail.com>
2020-03-25 08:05:22 +0100

    l1 bartender, drinks! kata added

---
+++ 8kyu/L1 Bartender, drinks!/index.js
@@ -0,0 +1,24 @@
+// Description:
+// Write a function getDrinkByProfession/get_drink_by_profession() that receives as input parameter a string, and produces outputs according to the following table:
+
+// Input	Output
+// "Jabroni"	"Patron Tequila"
+// "School Counselor"	"Anything with Alcohol"
+//  "Programmer"	 "Hipster Craft Beer"
+//  "Bike Gang Member"	"Moonshine" 
+//  "Politician"	"Your tax dollars" 
+//  "Rapper"	"Cristal" 
+//  *anything else*	"Beer" 
+// Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer."
+
+// Make sure you cover the cases where certain words do not show up with correct capitalization. For example, getDrinkByProfession("pOLitiCIaN") should still return "Your tax dollars".
+
+// My solution:
+const getDrinkByProfession = param => 
+  param.toLowerCase() === "jabroni" ? "Patron Tequila" :
+  param.toLowerCase() === "school counselor" ? "Anything with Alcohol":
+  param.toLowerCase() === "programmer"  ? "Hipster Craft Beer" :
+  param.toLowerCase() === "bike gang member"  ? "Moonshine" :
+  param.toLowerCase() === "politician" ? "Your tax dollars" :
+  param.toLowerCase() === "rapper"  ? "Cristal" :
+  "Beer"  
