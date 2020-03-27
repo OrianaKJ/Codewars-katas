cfbbdc1fcc9d0050ba91e02a2ddce7f035513832
Oriana <oriana.koziorynska@gmail.com>
2020-03-27 16:57:05 +0100

    numbers in different systems kata added

---
+++ 7kyu/Numbers in different systems/index.js
@@ -0,0 +1,11 @@
+// Description:
+// Translate number to the selected number system. If the number consists just of digits - return number, else - return string.
+
+//   SysNums(5,2) // 101
+//   SysNums(5,8) // 5
+//   SysNums(250,2) // 11111010
+//   SysNums(250,16) // "fa"
+// #####Hint: System can be 2, 8, 10, 16.
+
+// My solution:
+const sysNums = (n, sys) => isNaN(Number(n.toString(sys))) ? n.toString(sys) : Number(n.toString(sys))
