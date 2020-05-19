7d55250bfb6205e8ff0d163ad574c2fe5b1c6676
Oriana <oriana.koziorynska@gmail.com>
2020-05-19 06:54:03 +0200

    rgb to hex conversion kata added

---
+++ 5kyu/RGB To Hex Conversion/index.js
@@ -0,0 +1,25 @@
+// Description:
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+// My solution:
+const rgb = (r, g, b) => {
+    const conv = function (num) {
+        if (num < 0) {
+            return '00';
+        }
+        if (num > 255) {
+            return 'FF';
+        }
+        return (num > 15 ? num.toString(16) : '0' + num.toString(16)).toUpperCase();
+    }
+    return `${conv(r)}${conv(g)}${conv(b)}`;
+}
