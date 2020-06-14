bfda8c50d1136f4e858ef4e6eb3b9d7e2133f24d
Oriana <oriana.koziorynska@gmail.com>
2020-06-14 22:07:44 +0200

    halving sum kata added

---
+++ 7kyu/Halving Sum/index.js
@@ -0,0 +1,18 @@
+// Description:
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+// My solution:
+const halvingSum = n => {
+    let nums = [n]
+    while (n !== 0) {
+        nums.push(n = Math.floor(n / 2))
+    }
+    return nums.reduce((a, b) => a + b)
+}
