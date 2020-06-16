8f0a55f5c85cd086a7a2e7684c5b8db3b9a9ac95
Oriana <oriana.koziorynska@gmail.com>
2020-06-16 07:43:26 +0200

    moving zeros to the end kata added

---
+++ 5kyu/Moving Zeros To The End/index.js
@@ -0,0 +1,12 @@
+// Description:
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+// My solutions:
+
+// v1:
+// const moveZeros = arr => arr.reduceRight((accumulator, currentValue) => currentValue === 0 ? [...accumulator, currentValue] : [currentValue, ...accumulator], [])
+
+// v2:
+const moveZeros = (arr) => arr.filter(i => i !== 0).concat(arr.filter(i => i === 0));
