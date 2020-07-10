7a1b6826d336bd2901c09c6f9677638260d58c61
Oriana <oriana.koziorynska@gmail.com>
2020-07-10 06:31:14 +0200

    calculate average kata added

---
+++ 8kyu/Calculate average/index.js
@@ -0,0 +1,5 @@
+// Description:
+// Write function avg which calculates average of numbers in given list.
+
+// My solution:
+const find_average = array => array.reduce((a, b) => a + b, 0) / array.length
