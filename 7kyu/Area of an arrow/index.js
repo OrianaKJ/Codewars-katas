48194167fa0f871f1e68a7b6d8df23532e7afc51
Oriana <oriana.koziorynska@gmail.com>
2020-07-29 16:12:29 +0200

    area of an arrow kata added

---
+++ 7kyu/Area of an arrow/index.js
@@ -0,0 +1,10 @@
+// Description:
+// Area of an arrow
+// An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+// a and b are integers and > 0
+
+// Write a function which returns the area of the arrow.
+
+// My solution:
+const arrowArea = (a, b) => (0.5 * a) * (0.5 * b)
