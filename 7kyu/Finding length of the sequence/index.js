014aa826b59dce71a5c73f2102155ae379a4a1c6
Oriana <oriana.koziorynska@gmail.com>
2020-07-30 11:21:01 +0200

    finding length of the sequence kata added

---
+++ 7kyu/Finding length of the sequence/index.js
@@ -0,0 +1,20 @@
+// Description:
+// As part of this Kata, you need to find the length of the sequence in an array, between the first and the second occurrence of a specified number.
+
+// For example, for a given array arr
+
+// [0, -3, 7, 4, 0, 3, 7, 9]
+// Finding length between two 7s like
+
+// lengthOfSequence([0, -3, 7, 4, 0, 3, 7, 9], 7)
+// would return 5.
+
+// For sake of simplicity, there will only be numbers (positive or negative) in the supplied array.
+
+// If there are less or more than two occurrences of the number to search for, return 0, or in Haskell, Nothing.
+
+// My solution:
+const lengthOfSequence = (arr, n) => {
+    let occur = arr.filter(x => x == n).length
+    return occur > 2 || occur <= 1 ? 0 : arr.slice(arr.indexOf(n), arr.lastIndexOf(n)).length + 1
+}
