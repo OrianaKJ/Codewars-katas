ea530b5f1a9e5466d805c34d214766c2c742c884
Oriana <oriana.koziorynska@gmail.com>
2020-08-05 11:33:29 +0200

    find the capital leters kata added

---
+++ 7kyu/Find the capital leters/index.js
@@ -0,0 +1,8 @@
+// Description:
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+// My solution:
+const capitals = word => word.match(/[A-Z]/g).map(e => word.indexOf(e))
